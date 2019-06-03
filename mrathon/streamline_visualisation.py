605f12ac38182cb92c9be32f836a398f1e875414
Daniel Jorgens <danjorg@kth.se>
2019-06-03 17:05:13 +0200

    DEV: refactoring. Moved code from script into a visualiser class and restructured the package.

---
+++ mrathon/streamline_visualisation.py
@@ -0,0 +1,179 @@
+
+import numpy as np
+import nibabel as nib
+
+from queue import Queue
+from dipy.tracking.streamline import transform_streamlines
+from time import sleep
+from fury import window, actor
+
+
+class StreamlineVisualiser:
+
+    TIME_INC = 0.1
+
+    def __init__(self, plot_size, show, reset_camera, update_camera, window_size):
+
+        self.scene = window.Scene()
+        self.plot_size = plot_size
+        self.sm = window.ShowManager(scene=self.scene, size=plot_size, reset_camera=False)
+        self.show = show
+        self.reset_camera = reset_camera
+        self.update_camera = update_camera
+
+        self.pos_vec = np.array([0, 0, 0])
+        self.view_axis = np.array([0, 0, 0])
+
+        self.q = Queue(maxsize=window_size)
+
+        self.frames = []
+
+    @property
+    def window_size(self):
+        return self.q.maxsize
+
+    @property
+    def view_up(self):
+        return [0, 0, 1]
+
+    def add_and_render(self, streamlines, w):
+        """
+        Add given streamlines to the scene and render the update.
+
+        Parameters
+        ----------
+        streamlines : nib.streamlines.array_sequence.ArraySequence
+            streamlines to update the scene with
+        w : float
+            weighting factor for the new camera settings
+
+        Returns
+        -------
+        None
+        """
+
+        # remove oldest bunch of streamlines
+        if self.q.full():
+            self.scene.rm(self.q.get())
+
+        if self.show:
+            sleep(StreamlineVisualiser.TIME_INC)
+
+        # add new streamlines to the scene and the queue
+        sl_actor = actor.line(streamlines)
+        self.scene.add(sl_actor)
+        self.q.put(sl_actor)
+
+        # update camera settings
+        if self.update_camera:
+            self.apply_camera_update(streamlines, w)
+        if self.reset_camera:
+            self.scene.reset_camera()
+
+        # render updated scene or collect 2D snapshot of the scene
+        if self.show:
+            self.sm.render()
+        else:
+            self.frames.append(window.snapshot(self.scene, size=self.plot_size)[::-1, ::-1])
+
+    def apply_camera_update(self, streamlines, w):
+        """
+        Compute a camera update and perform it.
+
+        Parameters
+        ----------
+        streamlines : nib.streamlines.array_sequence.ArraySequence
+            streamlines based on which to perform the camera update
+        w : float
+            weight for the new camera settings
+
+        Returns
+        -------
+        None
+        """
+
+        # center of mass of the streamlines
+        center = np.mean(np.concatenate(streamlines, axis=0), axis=0)
+
+        # PCA-like description of the streamlines
+        cov = np.cov(np.concatenate(streamlines, axis=0).T)
+        evals, evecs = np.linalg.eig(cov)
+
+        idx = np.argsort(evals)[::-1]
+        evecs = evecs[:, idx]
+
+        # define the view axis as the axis with 'lowest variation' in the data (i.e. in the sl-points)
+        view = -1 * evecs[:, -1] if evecs[0, -1] < 0 else evecs[:, -1]
+
+        def weighting(vec, prev, _w):
+            return (1 - _w) * prev + _w * vec
+
+        # smoothly update the view axis on the data and the focal point, i.e. the center of the data
+        self.view_axis = weighting(view, self.view_axis, w)
+        self.pos_vec = weighting(center, self.pos_vec, w)
+
+        # update the camera settings
+        self.scene.set_camera(position=self.pos_vec + 80 * self.view_axis,
+                              focal_point=self.pos_vec,
+                              view_up=self.view_up)
+
+    def load_and_add_regions(self, region_list):
+        """
+        Load segmentations and add them to the scene as surfaces.
+
+        Parameters
+        ----------
+        region_list : list[str]
+            list of strings, each of which represents the path to a binary segmentation file (.nii)
+
+        Returns
+        -------
+        affine : 4x4 ndarray
+            Affine of all processed ROIs which are guaranteed to have the same affine.
+        """
+        affine = None
+
+        for region in region_list:
+
+            # load segmentation
+            seg = nib.load(region)
+
+            # check if affine matches the previous ones
+            if affine is not None:
+                if np.all(seg.affine != affine):
+                    raise ValueError('Non-matching affines!')
+            else:
+                affine = seg.affine
+
+            # add segmentation to the scene
+            seg_actor = actor.contour_from_roi(seg.get_data(), affine=seg.affine)
+            self.scene.add(seg_actor)
+
+        return affine
+
+    def load_and_add_reference_tractogram(self, path_to_tractogram):
+        """
+        Load tractogram from given path and add all streamlines to the scene.
+        The camera setup is determined based on this tractogram. The streamlines are visualised
+        with transparency.
+
+        Parameters
+        ----------
+        path_to_tractogram : str
+            path to tractogram file (.trk)
+
+        Returns
+        -------
+        None
+        """
+        # get streamlines in voxel space
+        tracto = nib.streamlines.load(path_to_tractogram)
+        streamlines = transform_streamlines(tracto.streamlines, np.linalg.inv(tracto.affine))
+
+        # add streamlines to the scene
+        sl_actor = actor.line(streamlines, opacity=0.3)
+        self.scene.add(sl_actor)
+
+        # update camera settings
+        self.apply_camera_update(streamlines, w=1)
+        self.scene.reset_camera()
--- vis_bundle.py
+++ vis_bundle.py
@@ -1,103 +1,21 @@
 #!/usr/bin/env python
 
-from time import sleep
 import numpy as np
 import nibabel as nib
-
-from fury import window, actor
-from fury.window import ShowManager
 
 from dipy.data import fetch_bundles_2_subjects, read_bundles_2_subjects
 from dipy.tracking.streamline import transform_streamlines
 
-from queue import Queue
 from imageio import mimsave
-
 from tqdm import tqdm
-
 from argparse import ArgumentParser, RawTextHelpFormatter
+
+
+from mrathon.streamline_visualisation import StreamlineVisualiser
 
 
 DESC = "Plot streamlines using dipy / fury."
 EPILOG = "---\n Author: danjorg@kth.se"
-
-
-TIME_INC = 0.1
-
-
-def add_and_render(_sm,  _scene, _streamlines, _q, prev_pos, prev_view, w_pos, reset, update_camera, _show):
-    if _show:
-        sleep(TIME_INC)
-    _sl_actor = actor.line(_streamlines)
-    _scene.add(_sl_actor)
-    _q.put(_sl_actor)
-    if update_camera:
-        _view_axis, _pos_vec = get_pos_vector(_streamlines, prev_pos, prev_view, w_pos)
-        _scene.set_camera(position=_pos_vec + 80*_view_axis,
-                          focal_point=_pos_vec,
-                          view_up=(0, 0, 1))
-    if reset:
-        _scene.reset_camera()
-    if _show:
-        _sm.render()
-
-    if update_camera:
-        return _view_axis, _pos_vec
-    else:
-        return None, None
-
-
-def get_pos_vector(_streamlines, prev_pos, prev_view, w_pos):
-    """
-
-    Parameters
-    ----------
-    w_pos : object
-    prev_view
-    prev_pos
-    _streamlines
-
-    Returns
-    -------
-    view_axis : 3-vector
-        best axis to look at the data
-    center : 3-vector
-        center of mass of the given streamlines
-    """
-
-    # center of data
-    center = np.mean(np.concatenate(_streamlines, axis=0), axis=0)
-
-    # automatic camera position
-    cov = np.cov(np.concatenate(_streamlines, axis=0).T)
-    evals, evecs = np.linalg.eig(cov)
-
-    idx = np.argsort(evals)[::-1]
-    evecs = evecs[:, idx]
-
-    view = -1 * evecs[:, -1] if evecs[0, -1] < 0 else evecs[:, -1]
-
-    def weighting(vec, prev, w):
-        return (1 - w) * prev + w * vec
-
-    return weighting(view, prev_view, w_pos), weighting(center, prev_pos, w_pos)
-
-
-def load_and_add_regions(region_list, _scene):
-
-    _affine = None
-
-    for region in region_list:
-        seg = nib.load(region)
-        if _affine is not None:
-            if np.all(seg.affine != _affine):
-                raise ValueError('Non-matching affines!')
-        else:
-            _affine = seg.affine
-        seg_actor = actor.contour_from_roi(seg.get_data(), affine=seg.affine)
-        _scene.add(seg_actor)
-
-    return _affine
 
 
 if __name__ == "__main__":
@@ -118,7 +36,9 @@ if __name__ == "__main__":
 
     args = vars(ap.parse_args())
 
-    scene = window.Scene()
+    sl_vis = StreamlineVisualiser(plot_size=args['plot_size'], show=args['outname'] is None,
+                                  update_camera=not args['reference_tractogram'], reset_camera=args['reset_camera'],
+                                  window_size=args['window_size'] // args['sl_per_frame'])
 
     if not args['tractogram']:
 
@@ -141,62 +61,25 @@ if __name__ == "__main__":
 
         # create static vis of a segmentation
         if args['regions']:
-            affine = load_and_add_regions(args['regions'], scene)
+            affine = sl_vis.load_and_add_regions(args['regions'])
             bundle_native = transform_streamlines(tracto.streamlines, np.linalg.inv(tracto.affine))
         else:
             bundle_native = transform_streamlines(tracto.streamlines, np.linalg.inv(tracto.affine))
 
-    sm = ShowManager(scene=scene, size=args['plot_size'], reset_camera=False)
-
-    maxsize = args['window_size'] // args['sl_per_frame']
-    q = Queue(maxsize=maxsize)
-
-    i = 0
     max_iter = len(bundle_native) // args['sl_per_frame']
-
-    frames = []
-    show = args['outname'] is None
-
-    pos_vec = np.array([0, 0, 0])
-    view_axis = np.array([0, 0, 0])
 
     # set camera based on reference streamlines
     if args['reference_tractogram']:
-        tracto = nib.streamlines.load(args['reference_tractogram'])
-        streamlines = transform_streamlines(tracto.streamlines, np.linalg.inv(tracto.affine))
-        sl_actor = actor.line(streamlines, opacity=0.3)
-        scene.add(sl_actor)
-        view_axis, pos_vec = get_pos_vector(streamlines, prev_view=view_axis, prev_pos=pos_vec, w_pos=1)
-        scene.set_camera(position=pos_vec + 80*view_axis,
-                         focal_point=pos_vec,
-                         view_up=(0, 0, 1))
-        scene.reset_camera()
+        sl_vis.load_and_add_reference_tractogram(args['reference_tractogram'])
         # scene.rm(sl_actor)
 
-    # fill the queue
-    while i < max_iter and not q.full():
-        view_axis, pos_vec = add_and_render(sm, scene,
-                                            bundle_native[i * args['sl_per_frame']:(i + 1) * args['sl_per_frame']], q,
-                                            prev_pos=pos_vec, prev_view=view_axis,
-                                            w_pos=args['initial_smoothness'], reset=args['reset_camera'], _show=show,
-                                            update_camera=not args['reference_tractogram'])
-        if not show:
-            frames.append(window.snapshot(scene, size=args['plot_size'])[::-1, ::-1])
-        i += 1
+    # vis and render
+    for i in tqdm(range(0, max_iter)):
+        sl_vis.add_and_render(bundle_native[i*args['sl_per_frame']:(i+1)*args['sl_per_frame']],
+                              w=args['smoothness'] if i > sl_vis.window_size else args['initial_smoothness'])
 
-    # update the queue
-    for i in tqdm(range(i, max_iter)):
-        scene.rm(q.get())  # remove oldest bunch of streamlines
-        view_axis, pos_vec = add_and_render(sm, scene,
-                                            bundle_native[i*args['sl_per_frame']:(i+1)*args['sl_per_frame']], q,
-                                            prev_pos=pos_vec, prev_view=view_axis,
-                                            w_pos=args['smoothness'], reset=args['reset_camera'], _show=show,
-                                            update_camera=not args['reference_tractogram'])
-        if not show:
-            frames.append(window.snapshot(scene, size=args['plot_size'])[::-1, ::-1])
-
-    if show:
-        sm.start()
+    if sl_vis.show:
+        sl_vis.sm.start()
 
     if args['outname']:
-        mimsave(args['outname'], frames, fps=args['fps'])
+        mimsave(args['outname'], sl_vis.frames, fps=args['fps'])
